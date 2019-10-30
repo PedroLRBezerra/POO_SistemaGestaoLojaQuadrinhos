3f09d2a1bcf77341d4cd4ae64849c123d4232be2
RajGod <pedro.luiz@hotmail.com.br>
2019-10-30 15:56:36 -0300

    add extends

--- src/view/ViewLogin.java
+++ src/view/ViewLogin.java
@@ -1,10 +1,19 @@
 package view;
 
-public class ViewLogin {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ViewLogin extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
