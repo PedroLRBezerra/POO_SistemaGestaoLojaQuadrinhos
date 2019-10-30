f65917cc6f246dfba9f348a91e57e43e5a13fff8
RajGod <pedro.luiz@hotmail.com.br>
2019-10-30 16:12:42 -0300

    Revert "add extends"
    This reverts commit 3f09d2a1bcf77341d4cd4ae64849c123d4232be2.

--- src/view/ViewLogin.java
+++ src/view/ViewLogin.java
@@ -1,19 +1,10 @@
 package view;
 
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-public class ViewLogin extends Application {
+public class ViewLogin {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-	}
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		
 	}
 
 }
