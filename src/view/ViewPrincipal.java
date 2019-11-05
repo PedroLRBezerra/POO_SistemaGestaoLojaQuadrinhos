dbafaf4025421ed03f1918b85c338ea9fdeb1340
RajGod <pedro.luiz@hotmail.com.br>
2019-11-05 17:34:00 -0300

    Update ViewPrincipal.java

--- src/view/ViewPrincipal.java
+++ src/view/ViewPrincipal.java
@@ -25,10 +25,10 @@ public class ViewPrincipal extends Application implements EventHandler<ActionEve
 	private MenuItem mnuClientes = new MenuItem("Clientes");
 	private MenuItem mnuTitulo = new MenuItem("Titulo");
 	private MenuItem mnuFuncionarios = new MenuItem("Funcionarios");
-	private MenuItem mnuEdicao = new MenuItem("Edição");
+	private MenuItem mnuEdicao = new MenuItem("EdiÃ§Ã£o");
 	private MenuItem mnuExemplar = new MenuItem("Exemplar");
 	private MenuItem mnuFolha = new MenuItem("Folha de pagamento");
-	private MenuItem mnuBalanco = new MenuItem("Balanço Geral");
+	private MenuItem mnuBalanco = new MenuItem("BalanÃ§o Geral");
 	
 	private BorderPane panPrincipal = new BorderPane();
 	
@@ -39,17 +39,17 @@ public class ViewPrincipal extends Application implements EventHandler<ActionEve
 	@Override
 	public void start(Stage stage) throws Exception {
 		panPrincipal.setTop(barraMenu);
-		panPrincipal.setStyle("-fx-padding: 10px");
+		//panPrincipal.setStyle("-fx-padding: 10px");
 		generateMenus();
 		Scene scn = new Scene(panPrincipal, 800, 600);
 		
 		stage.setScene(scn);
-		stage.setTitle("Gestão de Revistas");
+		stage.setTitle("GestÃ£o de Revistas");
 		stage.show();
 	}
 	
 	public void generateMenus() {
-		// criação da barra de menus 
+		// criaÃ§Ã£o da barra de menus 
 		barraMenu.getMenus().addAll(mnuRevista, mnuPessoa, mnuVenda,mnuRelatorio);
 		mnuRevista.getItems().addAll(mnuTitulo, mnuEdicao, mnuExemplar);
 		mnuPessoa.getItems().addAll(mnuClientes, mnuFuncionarios);
@@ -60,7 +60,7 @@ public class ViewPrincipal extends Application implements EventHandler<ActionEve
 		telas.put(mnuTitulo, new TituloBoundary());
 		telas.put(mnuEdicao, new EdicaoBoundary());
 		telas.put(mnuExemplar, new ExemplarBoundary());
-		//telas.put(mnuFuncionarios, new FuncionaroBoundary());
+		telas.put(mnuFuncionarios, new FuncionaroBoundary());
 		
 		Set<MenuItem> keys = telas.keySet();
 		for(MenuItem menu : keys) { 
